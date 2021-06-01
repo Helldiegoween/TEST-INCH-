a51e099db2bbe906cf38bd0c804b9eaa365ff78c
FabioMarin25 <85189608+FabioMarin25@users.noreply.github.com>
2021-06-01 12:57:46 -0500

    Add files via upload

---
+++ Main4.java
@@ -0,0 +1,10 @@
+import jdk.vm.ci.hotspot.HotSpotCallingConventionType;
+
+public class Main4 {
+    public static void main(String[] args) {
+        int x = 10 + 3;
+        int y = 15 * 3;
+        int z = x + y;
+        System.out.print(x);
+    }
+}
